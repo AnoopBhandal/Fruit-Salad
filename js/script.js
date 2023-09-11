ab16cc6f9c8c6cfdec31cd1202248693eecec1f9
AnoopBhandal <anoopbhandal1@gmail.com>
2023-09-11 12:06:45 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-const fruitForm =document.querySelector('#inputSection form')
-console.log(fruitForm)
+const fruitForm =document.querySelector('#inputSection form');
+
+const fruitList =document.querySelector("#fruitSection ul");
+
+const addFruit =(fruit) => {
+    const li = document.createElement("li")
+    li.textContent = fruit
+    fruitList.appendChild(li)   
+    li.addEventListener("click", ()=>{
+        fruitList.removeChild(li)
+    })
+}
+
+fruitForm.addEventListener("submit", e=> {
+    e.preventDefault()
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+});
+
